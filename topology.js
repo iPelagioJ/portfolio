5442ac6946b8db0976bbb7872d3aae87acb06daf
Jason <pelagiojason02@gmail.com>
2025-05-09 22:58:30 +0800

    Change background

---
+++ topology.js
@@ -0,0 +1,12 @@
+VANTA.TOPOLOGY({
+  el: "#vanta-container",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x667838,
+  backgroundColor: 0xcaffa8
+})
